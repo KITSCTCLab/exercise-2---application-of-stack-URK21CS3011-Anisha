15c17a41469fb5026832ffc271ad30a6c2fd0eeb
URK21CS3011-Anisha <109951728+URK21CS3011-Anisha@users.noreply.github.com>
2022-08-01 06:53:29 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -19,24 +19,28 @@ class Evaluate:
 
 
   def isEmpty(self):
+    return (self.top==-1)
     """
     Check whether the stack is empty.
     Returns:
       True if it is empty, else returns False.
     """
-      # Write your code here
+      
 
 
   def pop(self):
+    return (self.top==-1)
     """
     Do pop operation if the stack is not empty.
     Returns:
       The data which is popped out if the stack is not empty.
     """
-    # Write your code here
+      return (self.top==-1)
 
 
   def push(self, operand):
+     self.top+=1
+     self.stack[self.top]=operand
     """
     Push the operand to stack if the stack is not full.
     Arguments:
@@ -46,6 +50,30 @@ class Evaluate:
 
 
   def validate_postfix_expression(self, expression):
+    operatorcon=0
+    operandcon=0
+    flag=0
+    for i in expression:
+
+      if(not(i=="-" or i=="+" or i=="*" or i=="/" or i=="^")):
+
+        k=int(i)
+      if(i=="-" or i=="+" or i=="*" or i=="/" or i=="^"):
+
+        operatorcon+=1
+        flag=1
+      elif(isinstance(k,int)):    
+        operandcon+=1
+        flag=1
+      else:
+        flag=0
+        break
+    if(flag==1 and operandcon==(operatorcon+1)):
+      return True
+    else:
+      return False
+
+    
     """
     Check whether the expression is a valid postfix expression.
     Arguments:
@@ -57,6 +85,30 @@ class Evaluate:
 
 
   def evaluate_postfix_expression(self, expression):
+    flag=0
+    for i in expression:
+
+      if(not(i=="-" or i=="+" or i=="*" or i=="/" or i=="^")):
+
+        self.push(i)
+      else:
+        self.push(i)
+        operator=self.pop()
+        secoperator=self.pop()
+        if(operator=="+"):
+
+          self.stack[self.top]=int(self.stack[self.top])+int(secoperator)
+        elif(operator=="-"):
+          self.stack[self.top]=int(self.stack[self.top])-int(secoperator)
+        elif(operator=="*"):
+          self.stack[self.top]=int(self.stack[self.top])*int(secoperator)
+        elif(operator=="/"):
+          flag=1
+          self.stack[self.top]=int(self.stack[self.top])/int(secoperator)
+        else:
+          self.stack[self.top]=int(self.stack[self.top])^int(secoperator)
+    
+    return int(self.stack[self.top])
     """
     Evaluate the postfix expression
     Arguments:
