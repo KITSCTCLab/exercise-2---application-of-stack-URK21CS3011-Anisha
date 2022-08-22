780f96f580fc531b9a48b06aa59f985ca942139d
URK21CS3011-Anisha <109951728+URK21CS3011-Anisha@users.noreply.github.com>
2022-08-22 14:53:36 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,73 +7,59 @@ class Evaluate:
   """
     # Write your code here
 
-
-  def ___init___(self, size):
+  def __init__(self, size):
     """Inits Evaluate with top, size_of_stack and stack.
     Arguments:
       size_of_stack: An integer to set the size of stack.
     """
     self.top = -1
-    self.size_of_stack = size
-    self.stack = []
-
+    self.size = size
+    self.lst = [None]*size
 
   def isEmpty(self):
-    return (self.top==-1)
     """
     Check whether the stack is empty.
     Returns:
       True if it is empty, else returns False.
     """
-      
-
+    # Write your code here
+    if self.top == -1:
+       return 1
+    else :
+       return 0
+          
+  def is_full(self):
+    # Write code here
+    if self.top == (self.size - 1):
+      return 1
+    else :
+      return 0
 
   def pop(self):
-    return (self.top==-1)
     """
     Do pop operation if the stack is not empty.
     Returns:
       The data which is popped out if the stack is not empty.
     """
-      return (self.top==-1)
-
+    # Write your code here
+    if not self.isEmpty():
+      t=self.lst[self.top]
+      del self.lst[self.top]
+      self.top-=1
+      return t
 
   def push(self, operand):
-     self.top+=1
-     self.stack[self.top]=operand
     """
     Push the operand to stack if the stack is not full.
     Arguments:
       operand: The operand to be pushed.
     """
     # Write your code here
-
+    if not self.is_full():
+            self.top+=1
+            self.lst[self.top]=operand
 
   def validate_postfix_expression(self, expression):
-    operatorcon=0
-    operandcon=0
-    flag=0
-    for i in expression:
-
-      if(not(i=="-" or i=="+" or i=="*" or i=="/" or i=="^")):
-
-        k=int(i)
-      if(i=="-" or i=="+" or i=="*" or i=="/" or i=="^"):
-
-        operatorcon+=1
-        flag=1
-      elif(isinstance(k,int)):    
-        operandcon+=1
-        flag=1
-      else:
-        flag=0
-        break
-    if(flag==1 and operandcon==(operatorcon+1)):
-      return True
-    else:
-      return False
-
-    
     """
     Check whether the expression is a valid postfix expression.
     Arguments:
@@ -82,33 +68,19 @@ class Evaluate:
       True if the expression is valid, else returns False.
     """
     # Write your code here
-
+    c1=0
+    c2=0
+    for i in expression:
+      if i.isdigit():
+        c1+=1
+      else:
+        c2+=1
+    if c1>c2 and expression[0].isdigit() and expression[1].isdigit():
+      return 1
+    else:
+      return 0
 
   def evaluate_postfix_expression(self, expression):
-    flag=0
-    for i in expression:
-
-      if(not(i=="-" or i=="+" or i=="*" or i=="/" or i=="^")):
-
-        self.push(i)
-      else:
-        self.push(i)
-        operator=self.pop()
-        secoperator=self.pop()
-        if(operator=="+"):
-
-          self.stack[self.top]=int(self.stack[self.top])+int(secoperator)
-        elif(operator=="-"):
-          self.stack[self.top]=int(self.stack[self.top])-int(secoperator)
-        elif(operator=="*"):
-          self.stack[self.top]=int(self.stack[self.top])*int(secoperator)
-        elif(operator=="/"):
-          flag=1
-          self.stack[self.top]=int(self.stack[self.top])/int(secoperator)
-        else:
-          self.stack[self.top]=int(self.stack[self.top])^int(secoperator)
-    
-    return int(self.stack[self.top])
     """
     Evaluate the postfix expression
     Arguments:
@@ -117,7 +89,17 @@ class Evaluate:
       The result of evaluated postfix expression.
     """
     # Write your code here
-
+    for i in expression:
+      if i.isdigit():
+        self.push(i)
+      else:
+        v1 = self.pop()
+        v2 = self.pop()
+        if i=='/':
+          self.push(str(eval(v2 + i*2 + v1)))
+        else:
+          self.push(str(eval(v2 + i + v1)))
+    return self.pop()
 
 # Do not change the following code
 postfix_expression = input()  # Read postfix expression
